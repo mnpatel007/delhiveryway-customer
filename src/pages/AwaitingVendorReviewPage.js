9e007a3709de5023e6eac5082b45272e49cc89df
Meet <meetnp007@gmail.com>
2025-07-08 03:42:30 -0400

    update app

--- src/pages/AwaitingVendorReviewPage.js
+++ src/pages/AwaitingVendorReviewPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from './context/AuthContext';
+import { AuthContext } from '../context/AuthContext';
 import io from 'socket.io-client';
 import './AwaitingVendorReviewPage.css';
 
